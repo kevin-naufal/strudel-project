bf2c08ae0cb35e18b4ad9e2cca6c7392f8139198
Kevin Naufal Aryanto <kevinnaufal239@gmail.com>
2025-11-10 00:19:44 +0700

    Adding new project (Final Fantasy Prelude - Piano Version)

---
+++ ffprelude.js
@@ -0,0 +1,14 @@
+setcpm(1)
+
+// Final Fantasy Prelude - working piano arpeggio version
+$: note("c3 d3 e3 g3 c4 d4 e4 g4 c5 d5 e5 g5 c6 d6 e6 g6 c7 g6 e6 d6 c6 g5 e5 d5 c5 g4 e4 d4 c4 g3 e3 d3 \
+         c3 d3 e3 g3 c4 d4 e4 g4 c5 d5 e5 g5 c6 d6 e6 g6 c7 g6 e6 d6 c6 g5 e5 d5 c5 g4 e4 d4 c4 g3 e3 d3 \
+         a2 b2 c3 e3 a3 b3 c4 e4 a4 b4 c5 e5 a5 b5 c6 e6 a6 e6 c6 b5 a5 e5 c5 b4 a4 e4 c4 b3 a3 e3 c3 b2 \
+         c3 d3 e3 g3 c4 d4 e4 g4 c5 d5 e5 g5 c6 d6 e6 g6 c7 g6 e6 d6 c6 g5 e5 d5 c5 g4 e4 d4 c4 g3 e3 d3 \
+         a2 b2 c3 e3 a3 b3 c4 e4 a4 b4 c5 e5 a5 b5 c6 e6 a6 e6 c6 b5 a5 e5 c5 b4 a4 e4 c4 b3 a3 e3 c3 b2 \
+         a2 c3 f3 g3 a3 c4 f4 g4 a4 c5 f5 g5 a5 c6 f6 g6 a6 g6 f6 c6 a5 g5 f5 c5 a4 g4 f4 c4 a3 g3 f3 c3 \
+         b2 d3 g3 a3 b3 d4 g4 a4 b4 d5 g5 a5 b5 d6 g6 a6 b6 a6 g6 d6 b5 a5 g5 d5 b4 a4 g4 d4 b3 a3 g3 d3 \
+         ab2 c3 eb3 g3 ab3 c4 eb4 g4 ab4 c5 eb5 g5 ab5 c6 eb6 g6 ab6 g6 eb6 c6 ab5 g5 eb5 c5 ab4 g4 eb4 c4 ab3 g3 eb3 c3 \
+         bb2 d3 f3 a3 bb3 d4 f4 a4 bb4 d5 f5 a5 bb5 d6 f6 a6 bb6 a6 f6 d6 bb5 a5 f5 d5 bb4 a4 f4 d4 bb3 a3 f3 d3 \
+        ")
+   .sound("piano")
